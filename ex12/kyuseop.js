ecc2dca78843b458ae31ea8cd4bbf0f0d031d7ff
Jensen-Hwang <rbtjqghkd@gmail.com>
2022-10-06 22:18:28 +0900

    update | gyuseop | 10.06

--- ex12/kyuseop.js
+++ ex12/kyuseop.js
@@ -1,11 +1,13 @@
 function solution(p) {
   if (p.length === 0) return "";
+
   const idx = isBalance(p);
+
   let u = p.slice(0, idx + 1);
-  let v = p.slice(idx + 1) === undefined ? "" : p.slice(idx + 1);
+  let v = p.slice(idx + 1)
+
   if (isRight(u)) return u + solution(v)
   else return '(' + solution(v) + ')' + u.slice(1, -1).split("").map(reverse).join("")
-
 
 }
 
@@ -13,7 +15,6 @@ function reverse(x) {
   if (x === '(') return ')';
   else return '('
 }
-
 
 function isBalance(str) {
   const stack = [];
@@ -27,7 +28,6 @@ function isBalance(str) {
     if (stack.length === 0) return i;
   }
 }
-
 
 function isRight(str) {
   const stack = [];
